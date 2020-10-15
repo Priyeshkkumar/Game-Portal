3c4c123c5f76d4b6e4b0a88ba41c8761c9ad03bc
Priyeshkkumar <priyeshkkumar@gmail.com>
2020-10-15 16:53:47 +0530

    Updates: 1.Added code to connect to a MongoDB databae. 2.Minor updates in *.ejs

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
-const express = require("express");
 const express = require( "express" );
 const app = express();
+// Package to parse the POST route's data
+const bodyParser = require( "body-parser" );
+// Package to use uploaded files
+const fileUpload = require( "express-fileupload" );
+
+// Package to MongoDb
+const mongoose = require( "mongoose" );
+
+mongoose.connect( "mongodb://127.0.0.1:27017/testdb", {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}, function(error){
+    if( error ){
+        console.log(error);
+    }else{
+        console.log("Connected to database");
+    }
+} );
+
+// DB schema
+var gameSchema = new mongoose.Schema({
+    gameName: String,
+    creatorName: String,
+    gameHeight: Number,
+    gameWidth: Number,
+    gameFile: String,
+    thumbnailFile: String
+});
+
+var gameObj = mongoose.model( "gameObj", gameSchema );
 
 // To use content of public folder
 app.use( express.static ("public" ) );
@@ -9,12 +38,70 @@ app.use( express.static ("public" ) );
 
 // home page route
 app.get( "/", function( req, res ){
-    res.render( "homepage.ejs" );
+    gameObj.find( {}, function( error, ReturnedgameObj ){
+        if( error ){
+            console.log( "Pblm retrieving game data" );
+            console.log( error );
+        }
+        else{
+            res.render( "homepage.ejs",{
+                gamesList: ReturnedgameObj,
+            } );
+        }
+    } );
 } );
 
-// Add Games page route
+// Add Games GET route
 app.get( "/addgames", function( req, res ){
     res.render( "addgames.ejs" );
+} );
+
+// Using body-parser package
+app.use( bodyParser.urlencoded({extended: true }));
+app.use( bodyParser.json() );
+
+// Uisng file upload package
+app.use( fileUpload() );
+
+// Add Games POST route
+app.post( "/addgames", function( req, res ){
+    var gameData = req.body;
+    var gameFile = req.files.gameFile;
+    var thumbnailFile = req.files.thumbnailFile;
+
+    // Moving uploaded file to public/ folders
+    gameFile.mv( "public/games/" + gameFile.name, function( error ){
+        if( error ){
+            console.log("Cannot upload game file");
+        }else{
+            console.log("Game file uploaded");
+        }
+    } );
+    thumbnailFile.mv( "public/games/thumbnails/" + thumbnailFile.name, function( error ){
+        if( error ){
+            console.log("Cannot upload thumbnail file");
+        }else{
+            console.log("Thumbnail file uploaded");
+        }
+    } );
+
+    gameObj.create({
+        gameName: gameData.gameName,
+        creatorName: gameData.creatorName,
+        gameHeight: gameData.gameHeight,
+        gameWidth: gameData.gameWidth,
+        gameFile: gameFile.name,
+        thumbnailFile: thumbnailFile.name
+    }, function(error){
+        if(error){
+            console.log( "Pblm adding game to database" );
+        }else{
+            console.log( "Game Added Successfully" );
+        }
+    });
+
+    res.redirect("/");
+
 } );
 
 // 404 route
@@ -26,6 +113,6 @@ app.listen( 3000, function( error ){
     if(error){
         console.log(error);
     }else{
-        console.log("Server Started");
+        console.log("Server Started on port 3000");
     }
 } );
